db4b8ae37a08aa0b0775d0b5406368be8c7dc91b
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2021-12-26 17:21:15 -0500

    Create install script file

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# install NixOs (https://nixos.org/download.html)
+curl -L https://nixos.org/nix/install | sh
+
+# source nix
+source /home/mdupuis/.nix-profile/etc/profile.d/nix.sh
+
+
